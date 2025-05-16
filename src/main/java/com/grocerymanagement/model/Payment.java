caf05ff92198eb86784a398e8063d0976283cde4
IT24102306 Anushan L.K.D. <it24102306@my.sliit.lk>
2025-05-16 22:38:38 +0530

    Add Payment class with basic fields

--- src/main/java/com/grocerymanagement/model/Payment.java
+++ src/main/java/com/grocerymanagement/model/Payment.java
@@ -1,4 +1,14 @@
 package com.grocerymanagement.model;
 
-public class Payment {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment implements Serializable {
+    private String paymentId;
+    private Order order;
+    private BigDecimal amount;
+    private PaymentMethod paymentMethod;
+    private PaymentStatus status;
+    private LocalDateTime paymentDate;
 }

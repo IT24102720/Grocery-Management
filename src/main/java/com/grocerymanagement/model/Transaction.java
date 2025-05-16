a93de944be94a8cefe67a1f6bf4ba3967e1ea902
IT24102306 Anushan L.K.D. <it24102306@my.sliit.lk>
2025-05-16 23:05:10 +0530

    Edit transaction class

--- src/main/java/com/grocerymanagement/model/Transaction.java
+++ src/main/java/com/grocerymanagement/model/Transaction.java
@@ -1,4 +1,15 @@
 package com.grocerymanagement.model;
 
-public class Transaction {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class Payment implements Serializable {
+    private String paymentId;
+    private Order order;
+    private BigDecimal amount;
+    private Payment.PaymentMethod paymentMethod;
+    private Payment.PaymentStatus status;
+    private LocalDateTime paymentDate;
 }
